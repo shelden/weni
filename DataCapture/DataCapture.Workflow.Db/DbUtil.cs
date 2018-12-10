b29cb7b0c62a877b60671d607fb6196f4696399b
Brian Shelden <bshelden@wolve.com>
2018-12-09 20:41:54 -0500

    pulled in ConnectionFactory and DbUtil to D.W.Db

---
+++ DataCapture/DataCapture.Workflow.Db/DbUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+using System.Text;
+
+namespace VtPurple.Db
+{
+    public static class DbUtil
+    {
+        public static void AddParameter(IDbCommand command, String name, String value)
+        {
+            var param = command.CreateParameter();
+            param.DbType = System.Data.DbType.String;
+            param.ParameterName = name;
+            param.Value = value;
+            command.Parameters.Add(param);
+        }
+        public static void AddParameter(IDbCommand command, String name, int value)
+        {
+            var param = command.CreateParameter();
+            param.DbType = System.Data.DbType.Int32;
+            param.ParameterName = name;
+            param.Value = value;
+            command.Parameters.Add(param);
+        }
+        public static int SelectCount(IDbConnection dbConn, string table)
+        {
+ 
+            var command = dbConn.CreateCommand();
+            var sql = new StringBuilder();
+            sql.Append("SELECT COUNT(1) FROM ");
+            sql.Append(table);
+            command.CommandText = sql.ToString();
+            var o = command.ExecuteScalar();
+
+            // this is crap XXX RTFM how to get this scalar as int
+            String s = o.ToString();
+            return int.Parse(s);
+        }
+
+    }
+}
