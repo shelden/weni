b05366be8b66e9c78aa8fcdbbab92124c8cedaa8
Brian Shelden <shelden@vtpurple.com>
2019-01-02 11:48:57 -0500

    added test for two tempfiles being different

--- DataCapture/DataCapture.IO.Test/TempFileTest.cs
+++ DataCapture/DataCapture.IO.Test/TempFileTest.cs
@@ -25,6 +25,22 @@ namespace DataCapture.IO.Test
             }
             Assert.IsFalse(System.IO.File.Exists(copy));
         }
+
+        [Test()]
+        public void MultiplesAreDifferent()
+        {
+            String extension = "gobucks";
+            using (var tmp0 = new TempFile(extension))
+            {
+                using (var tmp1 = new TempFile(extension))
+                {
+                    Assert.IsTrue(System.IO.File.Exists(tmp0.FullName));
+                    Assert.IsTrue(System.IO.File.Exists(tmp1.FullName));
+                    Assert.AreNotEqual(tmp0.Value, tmp1.Value);
+                    Assert.AreNotEqual(tmp0.FullName, tmp1.FullName);
+                }
+            }
+        }
     }
 }
 
